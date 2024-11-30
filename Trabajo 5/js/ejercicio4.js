dc596c81826e22bff831c69992c3381f330ec701
romeromatias64 <135185736+romeromatias64@users.noreply.github.com>
2024-11-29 23:01:05 -0300

    Finalizado ejercicio 4

--- Trabajo 5/js/ejercicio4.js
+++ Trabajo 5/js/ejercicio4.js
@@ -1,3 +1,10 @@
 // 4. Pide al usuario dos valores y muestra cómo se intercambian entre sí. Por ejemplo, si ingresa a=5 y b=10, el resultado será a=10 y b=5. Los resultados se deben mostrar con un document.write donde se muestre si el user ingreso a=5 y b=10:
 // a = 10
 // b = 5
+
+function replacement() {
+    const a = prompt("Ingrese el número A:")
+    const b = prompt("Ingrese el número B:")
+    
+    document.write(`Los valores se han intercambiado entre si: <br> A = ${a.replace(a, b)} <br> B = ${b.replace(b, a)} <hr> Los valores originales eran: <br> A = ${a} <br> B = ${b}`)
+}
